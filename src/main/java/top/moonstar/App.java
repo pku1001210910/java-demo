211ec6040cb461f032ba5d0eadd4a31480f65ca4
xingxuwu <pku1001210910@163.com>
2020-10-03 03:02:04 +0000

    init repo
    Signed-off-by: xingxuwu <pku1001210910@163.com>

---
+++ src/main/java/top/moonstar/App.java
@@ -0,0 +1,13 @@
+package top.moonstar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {   
+        System.out.println( "Hello World, My Friend!" );
+    }
+}
