d3298b6f66ae0ed7104fc4d875e776a75c89915a
xingxu wu <pku1001210910@163.com>
2023-11-14 22:01:56 +0800

    add another problem
    Signed-off-by: xingxu wu <pku1001210910@163.com>

--- src/main/top/moonstar/string/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/top/moonstar/string/LongestSubstringWithoutRepeatingCharacters.java
@@ -2,8 +2,17 @@ package top.moonstar.string;
 
 public class LongestSubstringWithoutRepeatingCharacters {
     public static void main(String[] args) {
-        String s = "abcabcbb";
+        String s = "bbbbb";
         System.out.println(lengthOfLongestSubstring(s));
+    }
+
+    public static int findChar(String s, char ch, int left, int right) {
+        for (int i = left; i < right && i < s.length(); i++) {
+            if (ch == s.charAt(i)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     public static int lengthOfLongestSubstring(String s) {
@@ -11,9 +20,9 @@ public class LongestSubstringWithoutRepeatingCharacters {
 
         for (int left = 0, right = 0; right < s.length(); right++) {
             char ch = s.charAt(right);
-            int index = s.indexOf(ch, left, right);
+            int index = findChar(s, ch, left, right);
             if (index == -1) {
-                maxLength += 1;
+                maxLength = Math.max(maxLength, right - left + 1);
             } else {
                 left = index + 1;
             }
